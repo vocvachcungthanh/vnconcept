b1f2ad56aa8a6f29caaffb891b2fea59b47c6021
vocvahcungthanh <vocvachcungthanh@gmail.com>
2022-03-25 17:37:43 +0700

    Cap nhap chi tiet san pham

--- build/js/main.js
+++ build/js/main.js
@@ -12,4 +12,37 @@ $(document).ready(function () {
         });
     }
     sidebar();
+
+    function modal() {
+        $("[modal-show='show']").click(function () {
+            $($(this).attr("modal-data")).addClass("show-modal");
+            $($(this).attr("modal-data"))
+                .find(".content-modal")
+                .addClass("show-modal");
+            $("body").addClass("active-modal");
+        });
+        $("[modal-show='close']").click(function () {
+            setTimeout(function () {
+                $(".bs-modal").removeClass("show-modal");
+                $("body").removeClass("active-modal");
+            }, 500);
+            $(this)
+                .parents(".bs-modal")
+                .find(".content-modal")
+                .removeClass("show-modal");
+        });
+        let showModal = $(".content-modal");
+        $(".bs-modal").on("click", function (e) {
+            if (
+                !showModal.is(e.target) &&
+                showModal.has(e.target).length === 0
+            ) {
+                console.log("thanh");
+                $("body").removeClass("active-modal");
+                $(".bs-modal").removeClass("show-modal");
+            }
+        });
+    }
+
+    modal();
 });
